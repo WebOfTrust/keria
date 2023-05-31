7e22ff169b3272f1a9023774e0c0707b46978ad3
AlexAndrei98 <alexandrei1998@hotmail.it>
2023-05-30 21:21:03 -0500

    fix autocomplete

--- examples/signify-react-ts/tests/integrations.js
+++ examples/signify-react-ts/tests/integrations.js
@@ -4,10 +4,9 @@ import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 (async () => {
     puppeteer.use(StealthPlugin());
     const browser = await puppeteer.launch({
-        headless: false,
-        delay: 5050,
-        devtools: true,
-      });
+        // headless: false,
+        // headless: 'new',
+    });
     const page = await browser.newPage();
 
 
@@ -25,20 +24,23 @@ import StealthPlugin from 'puppeteer-extra-plugin-stealth';
     //open modal
 
     await page.goto('http://localhost:5173');
+    await page.setCacheEnabled(false);
 
     //open modal
     await page.click('.css-9b1tbl-MuiButtonBase-root-MuiButton-root')
 
-    await page.type('input[id=combo-box-demo]', 'http://localhost:3901', {delay: 10})
-    await page.keyboard.press('Enter');
+    await page.type('input[id=combo-box-demo]', 'http://localhost:390', { delay: 100 })
+    //enter key on the same input
+    await page.click('.MuiAutocomplete-listbox > li')
+
 
     //get button with class css-qzbt6i-MuiButtonBase-root-MuiIconButton-root-MuiAutocomplete-popupIndicator and click it 
     const button = await page.waitForSelector(
         '.css-nxo287-MuiInputBase-input-MuiOutlinedInput-input'
-        );
+    );
     await button.click();
 
-    await page.type('.css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input', '0123456789abcdefghijk',{delay: 10});
+    await page.type('.css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input', '0123456789abcdefghijk', { delay: 10 });
 
     // Wait and click on first result
     const searchResultSelector = '.css-sghohy-MuiButtonBase-root-MuiButton-root';
@@ -58,28 +60,16 @@ import StealthPlugin from 'puppeteer-extra-plugin-stealth';
     await page.waitForTimeout(1000)
     await bb[0].click();
 
-    // await page.click('button[aria-label="menu"]')
-    // await page.mouse.click(35, 35, { button: 'left' })
-    debugger
-    // const evala = await page.evaluate(() => {document.querySelector('button')});
-    // console.log(evala)
 
-    
 
     const clientRoute = await page.$x("//li[contains(., 'Client')]");
     await page.waitForTimeout(1000)
     await clientRoute[0].click();
-
-    // const agentData = await page.$x("//div[contains(., 'Agent')]");
-    // const rr = await agentData[0].evaluate((el) => document.querySelector('div').innerHTML);
-    // console.log(rr)
     console.log('here')
-    // const result = await page.waitForSelector('.identifiers')
-    const result = await page.evaluate(() => {document.querySelector('div').innerHTML}
-    );
-    console.log(result)
-
-    // await browser.close();
+    if (browser.headless == false) {
+        await page.waitForTimeout(5000) //wait for 5 seconds before closing
+    }
+    await browser.close();
     console.log(inner_html)
 
 })();
