a210d4b2597b3433842e3f4e5ea96054cb275afc
AlexAndrei98 <alexandrei1998@hotmail.it>
2023-05-30 21:49:20 -0500

    clean up integration test

--- examples/signify-react-ts/tests/integrations.js
+++ examples/signify-react-ts/tests/integrations.js
@@ -29,47 +29,56 @@ import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 
     //open modal
     await page.click('.css-9b1tbl-MuiButtonBase-root-MuiButton-root')
-
+    //fill beginning of url
     await page.type('input[id=combo-box-demo]', 'https:', { delay: 100 })
-    //enter key on the same input
+    //enter key on the same input 
     await page.click('.MuiAutocomplete-listbox > li')
-
-
-    //get button with class css-qzbt6i-MuiButtonBase-root-MuiIconButton-root-MuiAutocomplete-popupIndicator and click it 
-    const button = await page.waitForSelector(
-        '.css-nxo287-MuiInputBase-input-MuiOutlinedInput-input'
-    );
-    await button.click();
-
+    //fill passcode
     await page.type('.css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input', '0123456789abcdefghijk', { delay: 10 });
 
-    // Wait and click on first result
+    // click on the connect butto
     const searchResultSelector = '.css-sghohy-MuiButtonBase-root-MuiButton-root';
     await page.click(searchResultSelector);
 
     // page.on('console', msg => console.log('PAGE LOG:', msg.text()));
-
+    //STATUS: CONNECTED 
     const inner_html = await page.evaluate(() => document.querySelector('button:disabled').innerHTML);
-
-
-
+    //closte button 
     const Closebutton = await page.$x("//button[contains(., 'Close')]");
     await Closebutton[0].click();
-
-    //.css-1d6w9lk-MuiButtonBase-root-MuiIconButton-root 
+    //getting the menu button
     const bb = await page.$x("//button[@aria-label='menu']")
     await page.waitForTimeout(1000)
+    //clicking the menu button
     await bb[0].click();
-
-
-
+    //get client route
     const clientRoute = await page.$x("//li[contains(., 'Client')]");
     await page.waitForTimeout(1000)
     await clientRoute[0].click();
-    console.log('here')
-
-    await page.waitForTimeout(5000) //wait for 5 seconds before closing
-
+    console.log('going to identifiers')
+    await page.waitForTimeout(1000) //wait for 5 seconds before closing
+    //getting the menu button
+    const bb1 = await page.$x("//button[@aria-label='menu']")
+    //clicking the menu button
+    await bb1[0].click();
+    //get identifiers route
+    const IdentifiersRoute = await page.$x("//li[contains(., 'Ident')]");
+    await page.waitForTimeout(1000)
+    //click identifiers route
+    await IdentifiersRoute[0].click();
+    await page.waitForTimeout(1000)
+    //create new identifier
+    await page.click('.css-v8aa8k-MuiButtonBase-root-MuiButton-root')
+    await page.waitForTimeout(1000)
+    //rotate first identifier
+    const rotates = await page.$x("//button[contains(., 'Rotate')]");
+    await rotates[0].click();
+    await page.waitForTimeout(4000)
+    //show updated rotation
+    const update = await page.$x("//button[contains(., 'Update')]");
+    await update[0].click();
+    //close modal
+    await page.waitForTimeout(5000)
     await browser.close();
     console.log(inner_html)
 
