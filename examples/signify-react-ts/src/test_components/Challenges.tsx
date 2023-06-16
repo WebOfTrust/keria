3f9a9acdb32496a8db87142eb3f18f6fa8b56ddb
AlexAndrei98 <alexandrei1998@hotmail.it>
2023-06-16 00:42:38 -0500

    testing challenges

--- examples/signify-react-ts/src/test_components/Challenges.tsx
+++ examples/signify-react-ts/src/test_components/Challenges.tsx
@@ -20,20 +20,37 @@ export function Challenges() {
                     onClick={async () => {
                         try {
                             const url = "http://localhost:3901"
-                            const bran = '0123456789abcdefghijk'
-                            const client = new SignifyClient(url, bran)
-                            assert.equal(client.controller.pre, 'ELI7pg979AdhmvrjDeam2eAO2SR5niCgnjAJXJHtJose')
-                            await client.connect()
-                            const identifiers = client.identifiers()
-                            const ids = await identifiers.list_identifiers()
-                            assert.equal(ids[0].prefix,'EBcIURLpxmVwahksgrsGW6_dUw0zBhyEHYFk17eWrZfk')
+                            const bran1 = '1123456789abcdefghijk'
+                            const bran2 = '2123456789abcdefghijk'
+                            const client1 = new SignifyClient(url, bran1)
+                            // await client1.boot()
+                            await client1.connect()
+                            const identifiers1 = client1.identifiers()
+                            let challenges1 = client1.challenges()
+                            let challenge1_small = await challenges1.get_challenge(128)
+                            assert.equal(challenge1_small.words.length, 12)
+                            let challenge1_big = await challenges1.get_challenge(256)
+                            assert.equal(challenge1_big.words.length, 24)
+                            let op1 = await identifiers1.create('aid1', {})
+                            let aid1 = op1['response']
 
-                            let challenges = client.challenges()
-                            let challenge_small = await challenges.get_challenge(128)
-                            assert.equal(challenge_small.words.length, 12)
-                            let challenge_big = await challenges.get_challenge(256)
-                            assert.equal(challenge_big.words.length, 24)
+                            const client2 = new SignifyClient(url, bran2)
+                            // await client2.boot()
+                            await client1.connect()
+                            const identifiers2 = client2.identifiers()
+                            const challenges2 = client2.challenges()
+
+                            let op2 = await identifiers2.create('aid2', {})
+                            let aid2 = op2['response']
+
+                            let challenge_to_send = await challenges1.send_challenge('aid1',aid2,challenge1_small)
+                            console.log(challenge_to_send)
+
+                            let challenge_to_receive = await challenges2.accept_challenge('aid2',aid1,challenge_to_send)
+                            console.log(challenge_to_receive)
+
                             setTestResult("Passed")
+
                         }
                         catch (e) {
                             console.log(e)
