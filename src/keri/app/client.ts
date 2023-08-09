5e6836e234dadde76aeae51c9fd4deef1fdba6dd
Kevin Griffin <griffin.kev@gmail.com>
2023-08-09 15:28:31 -0400

    wip

--- src/keri/app/client.ts
+++ src/keri/app/client.ts
@@ -1,5 +1,8 @@
 
 
+/**
+ * Client
+ */
 export class Client {
     private readonly _url: string
 
@@ -7,24 +10,34 @@ export class Client {
         this._url = url
     }
 
-    url(path: string): string {
+    private url(path: string): string {
         let url = new URL(path, this._url)
 
         return url.href
     }
 
+    /**
+     * 
+     * @param name 
+     * @returns 
+     */
     getAccount(name: string): Promise<Response> {
         let url = this.url(`/account/${name}`)
         let req = new Request(url, {method: "GET"})
         return fetch(req)
     }
 
+    /**
+     * 
+     * @param name 
+     * @param key 
+     * @param ndig 
+     * @returns 
+     */
     createAccount(name: string, key: string, ndig: string): Promise<Response> {
         let url = this.url(`/account/${name}`)
         let body = {key, ndig}
         let req = new Request(url, {method: "POST", body: JSON.stringify(body)})
         return fetch(req)
     }
-
-
 }
