6cd0a10f3ee78bc3b98d8c7902c4bfb2de6cdb55
Petteri Stenius <petteri.stenius@gmail.com>
2023-09-08 18:40:40 +0300

    add --esm switch

--- examples/scripts/multisig-sigts.sh
+++ examples/scripts/multisig-sigts.sh
@@ -5,4 +5,4 @@ npx --package=signify-ts ts-node --esm client.ts
 
 read -n 1 -r -p "Press any key to create endpoints for multisig AID..."
 
-npx --package=signify-ts ts-node make_endroles.ts
+npx --package=signify-ts ts-node --esm make_endroles.ts
