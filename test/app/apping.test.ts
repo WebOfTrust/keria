8332406de6f41eb1814331ffbf91b69d71d6d4e6
Petteri Stenius <petteri.stenius@gmail.com>
2023-09-08 11:44:59 +0300

    array of string should be assignable to isith and nsith

--- test/app/apping.test.ts
+++ test/app/apping.test.ts
@@ -1,6 +1,7 @@
 import {strict as assert} from "assert";
 import {randomPasscode, randomNonce} from '../../src/keri/app/apping'
 import libsodium from "libsodium-wrappers-sumo"
+import {CreateIdentiferArgs, RotateIdentifierArgs} from "../../src/keri/app/signify";
 
 describe('Controller', () => {
     it('Random passcode', async () => {
@@ -15,6 +16,34 @@ describe('Controller', () => {
         assert.equal(nonce.length, 44)
     })
 
+    it('CreateIdentiferArgs', () => {
+        let args: CreateIdentiferArgs;
+        args = {
+            isith: 1,
+            nsith: 1
+        };
+        args = {
+            isith: "1",
+            nsith: "1"
+        };
+        args = {
+            isith: ["1"],
+            nsith: ["1"]
+        };
+    })
+
+    it('RotateIdentifierArgs', () => {
+        let args: RotateIdentifierArgs;
+        args = {
+            nsith: 1
+        };
+        args = {
+            nsith: "1"
+        };
+        args = {
+            nsith: ["1"]
+        };
+    })
 
 
 })
