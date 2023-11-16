316d79b0c2dde4970469db55ffa74dabc5f1ef92
lenkan <dlenksjo@gmail.com>
2023-11-16 14:55:01 +0100

    remove habname from exchange query

--- tests/peer/test_exchanging.py
+++ tests/peer/test_exchanging.py
@@ -118,12 +118,12 @@ def test_exchange_end(helpers):
         agent.exnseeker.index(exn.said)
 
         body = json.dumps({}).encode("utf-8")
-        res = client.simulate_post(f"/identifiers/aid1/exchanges/query", body=body)
+        res = client.simulate_post(f"/exchanges/query", body=body)
         assert res.status_code == 200
         assert len(res.json) == 2
 
         body = json.dumps({'filter': {'-i': pre}, 'sort': ['-dt']}).encode("utf-8")
-        res = client.simulate_post(f"/identifiers/aid1/exchanges/query", body=body)
+        res = client.simulate_post(f"/exchanges/query", body=body)
         assert res.status_code == 200
         assert len(res.json) == 2
 
@@ -136,7 +136,7 @@ def test_exchange_end(helpers):
         assert serder.said == exn.said
 
         body = json.dumps({'filter': {'-i': pre}, 'sort': ['-dt'], 'skip': 1, "limit": 1}).encode("utf-8")
-        res = client.simulate_post(f"/identifiers/aid1/exchanges/query", body=body)
+        res = client.simulate_post(f"/exchanges/query", body=body)
         assert res.status_code == 200
         assert len(res.json) == 1
 
@@ -144,7 +144,7 @@ def test_exchange_end(helpers):
         serder = coring.Serder(ked=ked)
         assert serder.said == exn.said
 
-        res = client.simulate_get(f"/identifiers/aid1/exchanges/{exn.said}")
+        res = client.simulate_get(f"/exchanges/{exn.said}")
         assert res.status_code == 200
         serder = coring.Serder(ked=res.json['exn'])
         assert serder.said == exn.said
@@ -179,7 +179,7 @@ def test_exchange_end(helpers):
         agent.exnseeker.index(exn.said)
 
         body = json.dumps({'sort': ['-dt']}).encode("utf-8")
-        res = client.simulate_post(f"/identifiers/aid1/exchanges/query", body=body)
+        res = client.simulate_post(f"/exchanges/query", body=body)
         assert res.status_code == 200
         assert len(res.json) == 3
 
