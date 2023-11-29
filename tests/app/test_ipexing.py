d0df6e05394fe2ba15b35576dfa559e3ce4021c5
pfeairheller <pfeairheller@gmail.com>
2023-11-28 19:46:11 -0800

    Additional test coverage.
    Signed-off-by: pfeairheller <pfeairheller@gmail.com>

--- tests/app/test_ipexing.py
+++ tests/app/test_ipexing.py
@@ -8,11 +8,15 @@ Testing credentialing endpoint in the Mark II Agent
 import json
 
 from falcon import testing
+from hio.base import doing
+from keri.app import habbing, signing
 from keri.core import eventing, coring
 from keri.help import helping
 from keri.peer import exchanging
+from keri.vc import proving
 
 from keria.app import ipexing, aiding
+from keria.app.credentialing import CredentialResourceEnd
 
 
 def test_load_ends(helpers):
@@ -25,6 +29,8 @@ def test_load_ends(helpers):
 
         (end, *_) = app._router.find("/identifiers/NAME/ipex/admit")
         assert isinstance(end, ipexing.IpexAdmitCollectionEnd)
+        (end, *_) = app._router.find("/identifiers/NAME/ipex/grant")
+        assert isinstance(end, ipexing.IpexGrantCollectionEnd)
 
 
 def test_ipex_admit(helpers, mockHelpingNowIso8601):
@@ -91,7 +97,7 @@ def test_ipex_admit(helpers, mockHelpingNowIso8601):
             rec=[pre1]
         )
 
-        #Bad Sender
+        # Bad Sender
         data = json.dumps(body).encode("utf-8")
         res = client.simulate_post(path="/identifiers/BAD/ipex/admit", body=data)
         assert res.status_code == 404
@@ -157,3 +163,131 @@ def test_ipex_admit(helpers, mockHelpingNowIso8601):
         assert res.status_code == 202
         assert len(agent.exchanges) == 2
         assert len(agent.admits) == 1
+
+
+def test_ipex_grant(helpers, mockHelpingNowIso8601, seeder):
+    salt = b'0123456789abcdef'
+
+    with helpers.openKeria() as (agency, agent, app, client), \
+            habbing.openHab(name="issuer", salt=salt, temp=True) as (issuerHby, issuerHab), \
+            helpers.withIssuer(name="issuer", hby=issuerHby) as issuer:
+
+        client = testing.TestClient(app)
+        seeder.seedSchema(agent.hby.db)
+        seeder.seedSchema(issuerHby.db)
+
+        grantAnd = ipexing.IpexGrantCollectionEnd()
+        app.add_route("/identifiers/{name}/ipex/grant", grantAnd)
+
+        end = aiding.IdentifierCollectionEnd()
+        app.add_route("/identifiers", end)
+
+        salt2 = b'0123456789abcdeg'
+        op = helpers.createAid(client, "legal-entity", salt2)
+        le = op["response"]
+        pre1 = le['i']
+        assert pre1 == "EFnYGvF_ENKJ_4PGsWsvfd_R6m5cN-3KYsz_0mAuNpCm"
+
+        salt3 = b'0123456789abc123'
+        op = helpers.createAid(client, "verifier", salt3)
+        verifier = op["response"]
+        pre1 = verifier['i']
+        assert pre1 == "EEtaMHCGi83N3IJN05DRDhkpIo5S03LOX5_8IgdvMaVq"
+
+        # Lets issue a QVI credential to the QVI
+        issuer.createRegistry(issuerHab.pre, name="issuer")
+        qvisaid = issuer.issueQVIvLEI("issuer", issuerHab, le['i'], "78I9GKEFM361IFY3PIN0")
+
+        ims = CredentialResourceEnd.outputCred(issuer.hby, issuer.rgy, qvisaid)
+
+        agent.parser.parse(ims)
+
+        creder, prefixer, seqner, saider = agent.rgy.reger.cloneCred(said=qvisaid)
+        acdc = signing.serialize(creder, prefixer, seqner, saider)
+
+        iss = next(agent.rgy.reger.clonePreIter(pre=creder.said))
+        anc = next(agent.hby.db.clonePreIter(pre=issuerHab.pre, fn=1))
+        embeds = dict(
+            acdc=acdc,
+            iss=iss,
+            anc=anc
+        )
+
+        exn, end = exchanging.exchange(route="/ipex/grant",
+                                       payload=dict(),
+                                       sender=le['i'],
+                                       embeds=embeds,
+                                       recipient=verifier['i'],
+                                       date=helping.nowIso8601())
+        assert exn.ked == {'a': {'i': 'EEtaMHCGi83N3IJN05DRDhkpIo5S03LOX5_8IgdvMaVq'},
+                           'd': 'EHwjDEsub6XT19ISLft1m1xMNvVXnSfH0IsDGllox4Y8',
+                           'dt': '2021-06-27T21:26:21.233257+00:00',
+                           'e': {'acdc': {'a': {'LEI': '78I9GKEFM361IFY3PIN0',
+                                                'd': 'ELJ7Emhi0Bhxz3s7HyhZ45qcsgpvsT8p8pxwWkG362n3',
+                                                'dt': '2021-06-27T21:26:21.233257+00:00',
+                                                'i': 'EFnYGvF_ENKJ_4PGsWsvfd_R6m5cN-3KYsz_0mAuNpCm'},
+                                          'd': 'EBg1YzKmwZIDzZsMslTFwQARB6nUN85sRJF5oywlJr3N',
+                                          'i': 'EIqTaQiZw73plMOq8pqHTi9BDgDrrE7iE9v2XfN2Izze',
+                                          'ri': 'EACehJRd0wfteUAJgaTTJjMSaQqWvzeeHqAMMqxuqxU4',
+                                          's': 'EFgnk_c08WmZGgv9_mpldibRuqFMTQN-rAgtD-TCOwbs',
+                                          'v': 'ACDC10JSON000197_'},
+                                 'anc': {'a': [{'d': 'EACehJRd0wfteUAJgaTTJjMSaQqWvzeeHqAMMqxuqxU4',
+                                                'i': 'EACehJRd0wfteUAJgaTTJjMSaQqWvzeeHqAMMqxuqxU4',
+                                                's': '0'}],
+                                         'd': 'EJd2vLCnlcIb4ZLOhSHZOag4_FD-pxI96-r7e6_FT7CU',
+                                         'i': 'EIqTaQiZw73plMOq8pqHTi9BDgDrrE7iE9v2XfN2Izze',
+                                         'p': 'EIqTaQiZw73plMOq8pqHTi9BDgDrrE7iE9v2XfN2Izze',
+                                         's': '1',
+                                         't': 'ixn',
+                                         'v': 'KERI10JSON00013a_'},
+                                 'd': 'EKE374o9DAg9GIiFaDzk0g85sx2IV89cA8Iu4E_84Vug',
+                                 'iss': {'d': 'EO83mwXWqiGxovpTXE6QQUBP05xkP9c1xc88xvMwkWWZ',
+                                         'dt': '2021-06-27T21:26:21.233257+00:00',
+                                         'i': 'EBg1YzKmwZIDzZsMslTFwQARB6nUN85sRJF5oywlJr3N',
+                                         'ri': 'EACehJRd0wfteUAJgaTTJjMSaQqWvzeeHqAMMqxuqxU4',
+                                         's': '0',
+                                         't': 'iss',
+                                         'v': 'KERI10JSON0000ed_'}},
+                           'i': 'EFnYGvF_ENKJ_4PGsWsvfd_R6m5cN-3KYsz_0mAuNpCm',
+                           'p': '',
+                           'q': {},
+                           'r': '/ipex/grant',
+                           't': 'exn',
+                           'v': 'KERI10JSON000517_'}
+        assert end == (b'-LAg4AACA-e-acdc-IABEBg1YzKmwZIDzZsMslTFwQARB6nUN85sRJF5oywlJr3N'
+                       b'0AAAAAAAAAAAAAAAAAAAAAAAEO83mwXWqiGxovpTXE6QQUBP05xkP9c1xc88xvMw'
+                       b'kWWZ-LAW5AACAA-e-iss-VAS-GAB0AAAAAAAAAAAAAAAAAAAAAACEKZtbklUNPLO'
+                       b'f9soxY6nLGAbqCDDfEMJRvJQfpcoYUdW-LAr5AACAA-e-anc-VAn-AABAAB8FdrC'
+                       b'kf1kImQ8zRvKNWv2X_yElspb6bJ7eMg1B6Ly6wyLcDlfAkK5NnyB_qUaGVSilz63'
+                       b'D2n4mJ8w_8AAo2wN-EAB0AAAAAAAAAAAAAAAAAAAAAAB1AAG2021-06-27T21c26'
+                       b'c21d233257p00c00')
+        sigs = ["AAAa70b4QnTOtGOsMqcezMtVzCFuRJHGeIMkWYHZ5ZxGIXM0XDVAzkYdCeadfPfzlKC6dkfiwuJ0IzLOElaanUgH"]
+
+        body = dict(
+            exn=exn.ked,
+            sigs=sigs,
+            atc="",
+            rec=["EZ-i0d8JZAoTNZH3ULaU6JR2nmwyvYAfSVPzhzS6b5CM"]
+        )
+
+        data = json.dumps(body).encode("utf-8")
+        res = client.simulate_post(path="/identifiers/test/ipex/grant", body=data)
+        assert res.status_code == 404
+
+        res = client.simulate_post(path="/identifiers/legal-entity/ipex/grant", body=data)
+        assert res.status_code == 400
+        assert res.json == {'description': 'attempt to send to unknown '
+                                           'AID=EZ-i0d8JZAoTNZH3ULaU6JR2nmwyvYAfSVPzhzS6b5CM',
+                            'title': '400 Bad Request'}
+
+        body = dict(
+            exn=exn.ked,
+            sigs=sigs,
+            atc="",
+            rec=[verifier['i']]
+        )
+
+        data = json.dumps(body).encode("utf-8")
+        res = client.simulate_post(path="/identifiers/legal-entity/ipex/grant", body=data)
+        assert res.status_code == 202
+        assert res.json == exn.ked
