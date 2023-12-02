5ab8b3ddd581fc585c1b2c90a84bfcb756802436
Petteri Stenius <petteri.stenius@gmail.com>
2023-12-02 10:26:33 +0200

    cleanup

--- examples/integration-scripts/singlesig-dip.test.ts
+++ examples/integration-scripts/singlesig-dip.test.ts
@@ -19,31 +19,35 @@ beforeAll(async () => {
 
 describe("singlesig-dip", () => {
     test("delegate1a", async () => {
+        // delegate creates identifier without witnesses
         let kargs: CreateIdentiferArgs = {
             delpre: name1_id
         };
         let result = await client2.identifiers().create("delegate1", kargs);
         let op = await result.op();
-        let dip1 = await client2.identifiers().get("delegate1");
-        expect(op.name).toEqual(`delegation.${dip1.prefix}`);
+        let delegate1 = await client2.identifiers().get("delegate1");
+        expect(op.name).toEqual(`delegation.${delegate1.prefix}`);
     });
     test("delegator1", async () => {
-        let dip1 = await client2.identifiers().get("delegate1");
+        // delegator approves delegate
+        let delegate1 = await client2.identifiers().get("delegate1");
         let seal = {
-            i: dip1.prefix,
+            i: delegate1.prefix,
             s: 0,
-            d: dip1.prefix
+            d: delegate1.prefix
         };
         let result = await client1.identifiers().interact("name1", seal);
         let op = waitOperation(client1, await result.op());
     })
     test("delegate1b", async () => {
-        let dip1 = await client2.identifiers().get("delegate1");
-        let op: any = { name: `delegation.${dip1.prefix}` };
+        // delegate waits for completion
+        let delegate1 = await client2.identifiers().get("delegate1");
+        let op: any = { name: `delegation.${delegate1.prefix}` };
         op = await waitOperation(client2, op);
-        expect(dip1.prefix).toEqual(op.response.i);
+        expect(delegate1.prefix).toEqual(op.response.i);
     });
     test("delegate2a", async () => {
+        // delegate creates identifier with default witness config
         let env = resolveEnvironment();
         let kargs: CreateIdentiferArgs = {
             delpre: name1_id,
@@ -52,23 +56,26 @@ describe("singlesig-dip", () => {
         };
         let result = await client2.identifiers().create("delegate2", kargs);
         let op = await result.op();
-        let dip1 = await client2.identifiers().get("delegate2");
-        expect(op.name).toEqual(`delegation.${dip1.prefix}`);
+        let delegate2 = await client2.identifiers().get("delegate2");
+        expect(op.name).toEqual(`delegation.${delegate2.prefix}`);
     });
     test("delegator2", async () => {
-        let dip1 = await client2.identifiers().get("delegate2");
+        // delegator approves delegate
+        let delegate2 = await client2.identifiers().get("delegate2");
         let seal = {
-            i: dip1.prefix,
+            i: delegate2.prefix,
             s: 0,
-            d: dip1.prefix
+            d: delegate2.prefix
         };
         let result = await client1.identifiers().interact("name1", seal);
         let op = waitOperation(client1, await result.op());
     })
-    test("delegate2b", async () => {
-        let dip1 = await client2.identifiers().get("delegate2");
-        let op: any = { name: `delegation.${dip1.prefix}` };
+    // https://github.com/WebOfTrust/signify-ts/issues/145
+    test.failing("delegate2b", async () => {
+        // delegate waits for completion
+        let delegate2 = await client2.identifiers().get("delegate2");
+        let op: any = { name: `delegation.${delegate2.prefix}` };
         op = await waitOperation(client2, op);
-        expect(dip1.prefix).toEqual(op.response.i);
-    });
+        expect(delegate2.prefix).toEqual(op.response.i);
+    }, 30000);
 });
