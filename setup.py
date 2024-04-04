a754a8ac76f7021fd64655a6866845173b2f69c3
s-a-tanjim <shoebtanjim@outlook.com>
2024-04-04 15:13:34 +0600

    Increase timeout workaround in keripy [Dev only]

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setup(
     python_requires='>=3.10.4',
     install_requires=[
         'hio>=0.6.9',
-        'keri>=1.1.6',
+        'keri @ git+https://git@github.com/provenant-dev/keripy.git@v1.1.6-pr1',
         'mnemonic>=0.20',
         'multicommand>=1.0.0',
         'falcon>=3.1.0',
