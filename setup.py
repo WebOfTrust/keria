fcaea50094ec82a14674073da91f54332c1ba40d
2byrds <2byrds@gmail.com>
2024-05-29 16:50:42 -0400

    pointing to latest keri with fix, will point to tag when available
    Signed-off-by: 2byrds <2byrds@gmail.com>

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setup(
     python_requires='>=3.12.2',
     install_requires=[
         'hio>=0.6.12',
-        'keri==https://github.com/WebOfTrust/keripy',
+        'keri @ git+https://github.com/WebOfTrust/keripy.git@main',
         'mnemonic>=0.20',
         'multicommand>=1.0.0',
         'falcon>=3.1.3',
