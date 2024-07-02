0bde30f1f760e0a95189f523687dbca3c0f79c01
Rodolfo Miranda <rodolfo.miranda@gmail.com>
2024-07-01 21:52:20 -0300

    prefixes allowed in identifiers endpoint

--- tests/app/test_aiding.py
+++ tests/app/test_aiding.py
@@ -854,7 +854,7 @@ def test_identifier_collection_end(helpers):
                 }
         res = client.simulate_post(path="/identifiers/randybad/events", body=json.dumps(body))
         assert res.status_code == 404
-        assert res.json == {'title': 'No AID with name randybad found'}
+        assert res.json == {'title': 'No AID with name or prefix randybad found'}
 
         body = {
             'sigs': sigers,
@@ -1329,7 +1329,7 @@ def test_identifier_resource_end(helpers):
 
         res = client.simulate_get(path="/identifiers/bad")
         assert res.status_code == 404
-        assert res.json == {'description': 'bad is not a valid identifier name', 'title': '404 Not Found'}
+        assert res.json == {'description': 'bad is not a valid identifier name or prefix', 'title': '404 Not Found'}
 
         res = client.simulate_get(path="/identifiers/aid1")
         assert res.status_code == 200
