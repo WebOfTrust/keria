0906221921f85b00b151d774482951394d39ead0
Rodolfo Miranda <rodolfo.miranda@gmail.com>
2024-08-21 10:29:17 -0300

    fix hio ver

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ setup(
     ],
     python_requires='>=3.12.2',
     install_requires=[
-        'hio>=0.6.12',
+        'hio>=0.6.14',
         'keri==1.2.0.dev11',
         'mnemonic>=0.21',
         'multicommand>=1.0.0',
