ce864ef80bd5d9af553c8976b460bebe1fd94107
Rodolfo Miranda <rodolfo.miranda@gmail.com>
2024-09-25 11:12:44 -0400

    fix int

--- src/keria/app/agenting.py
+++ src/keria/app/agenting.py
@@ -781,7 +781,7 @@ class Escrower(doing.Doer):
 
 class Releaser(doing.Doer):
     KERIAReleaserTimeOut = "KERIA_RELEASER_TIMEOUT"
-    TimeoutRel = os.getenv(KERIAReleaserTimeOut) or 1*60*60
+    TimeoutRel = int(os.getenv(KERIAReleaserTimeOut)) or 1*60*60
     def __init__(self, agency):
         """ Check open agents and close if idle for more than TimeoutRel seconds
 
